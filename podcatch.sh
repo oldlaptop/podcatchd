f16e66aa9a48a5cce75142722ecd751241aa1357
oldlaptop <oldlaptop654@aol.com>
2020-01-02 11:00:19 -0500

    preload new podcast directories from the entire feed  when -n 1 was added, I think it still just blindly downloaded each file without  checking for existence

--- podcatch.sh
+++ podcatch.sh
@@ -71,7 +71,7 @@ download_loop()
 mkdir -m 700 "/tmp/podcatch.sh.$$"
 mkfifo /tmp/podcatch.sh.$$/rss.fifo
 
-rsstail -n 1 -Ppeu "$1" > /tmp/podcatch.sh.$$/rss.fifo 2>&1 & pids="$! $pids"
+rsstail -Ppeu "$1" > /tmp/podcatch.sh.$$/rss.fifo 2>&1 & pids="$! $pids"
 download_loop $! $1 < /tmp/podcatch.sh.$$/rss.fifo & pids="$! $pids"
 
 trap 'kill $pids; rm -rf /tmp/podcatch.sh.$$' EXIT
